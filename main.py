29e1710a90467fb48edd38614743af044f9b017a
sergiosentry <109162568+serglom21@users.noreply.github.com>
2023-11-20 15:41:25 -0500

    adding error handling

--- main.py
+++ main.py
@@ -3,34 +3,42 @@ import os
 from dotenv import load_dotenv
 
 def main():
-    load_dotenv()
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization" : "Bearer " + os.environ["AUTH_TOKEN"]
-    }
-    projects_response = requests.get(get_projects_url(), headers = headers)
-    if projects_response is not None and projects_response.status_code == 200:
-        projects = projects_response.json()
-        for project in projects:
-            key_response = requests.get(get_keys_url(project["slug"]), headers = headers)
-            if key_response is not None and key_response.status_code == 200:
-                keys = key_response.json()
-                payload = {
-                    "rateLimit" : {
-                        "window" : os.environ["RATELIMIT_WINDOW"], # 86400 seconds in a day
-                        "count" : os.environ["RATELIMIT_COUNT"]
+    try:
+        load_dotenv()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization" : "Bearer " + os.environ["AUTH_TOKEN"]
+        }
+        projects_response = requests.get(get_projects_url(), headers = headers)
+        if projects_response is not None and projects_response.status_code == 200:
+            projects = projects_response.json()
+            for project in projects:
+                key_response = requests.get(get_keys_url(project["slug"]), headers = headers)
+                if key_response is not None and key_response.status_code == 200:
+                    keys = key_response.json()
+                    payload = {
+                        "rateLimit" : {
+                            "window" : os.environ["RATELIMIT_WINDOW"],
+                            "count" : os.environ["RATELIMIT_COUNT"]
+                        }
                     }
-                }
-                for key in keys:
-                    print(f'About to update {project["slug"]}/ key = {key["id"]}')
-                    update_response = requests.put(url = get_update_key_url(project["slug"], key["id"]), json = payload, headers = headers)
-                    if update_response is not None and update_response.status_code in [200,201]:
-                        print("success")
-                        print(f'{project["slug"]}/ key = {key["id"]}')
-                    else:
-                        print("failed")
-                        print(update_response.json())
+                    for key in keys:
+                        print(f'About to update {project["slug"]}/ key = {key["id"]}')
+                        update_response = requests.put(url = get_update_key_url(project["slug"], key["id"]), json = payload, headers = headers)
+                        if update_response is not None and update_response.status_code in [200,201]:
+                            print("Key update success")
+                            print(f'{project["slug"]}/ key = {key["id"]}')
+                        else:
+                            print("Key update failed")
+                            print(update_response.json())
 
+                else:
+                    print(f'Could not find keys for project {project["slug"]}')
+        else:
+            print(f'Could not fetch projects for org {os.environ["ORG_SLUG"]}')
+            print(projects_response.json())
+    except Exception as e:
+        print(f'Something went wrong: {str(e)}')
 
 def get_projects_url():
     return f'https://sentry.io/api/0/organizations/{os.environ["ORG_SLUG"]}/projects/'
